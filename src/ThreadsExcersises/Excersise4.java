b642ee36ea0090ea0d2f6d2da9d301b537bed473
DarkDrinker <Anders.wade.jensen@hotmail.com>
2024-02-09 00:05:32 +0100

    Almost done

--- src/ThreadsExcersises/Excersise4.java
+++ src/ThreadsExcersises/Excersise4.java
@@ -5,13 +5,19 @@ import java.util.concurrent.Executors;
 
 public class Excersise4 {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool();
+        // Determine the number of available processors
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
 
-        for (char ch = 'A'; ch <= 'Z'; ch++) {
-            final char finalCh = ch;
+        //Create executerservice based on how many cores we have in pc
+        ExecutorService executorService = Executors.newFixedThreadPool(availableProcessors);
+
+        // Submit tasks to print ints to 10.000 or more for longer load??
+        for (int ch = 0; ch <= 10000 ; ch++) {
+            final int finalCh = ch;
             executorService.submit(() -> System.out.println(Thread.currentThread().getName() + ": " + finalCh));
         }
 
+        // Shutdown the ExecutorService
         executorService.shutdown();
     }
 }
